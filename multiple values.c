b29c900b3fde7642b2336ca62c43b917f4c257b6
sahil258 <70344390+sahil258@users.noreply.github.com>
2020-08-28 01:47:14 +0530

    Add files via upload
    thanks for unschool.in

---
+++ multiple values.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("sum of %d+%d+%d is %d",a,b,c,d);
+
+    return 0;
+}
